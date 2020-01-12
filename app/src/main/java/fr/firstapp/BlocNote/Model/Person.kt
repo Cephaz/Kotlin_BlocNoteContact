d190c2b20cc76df05294c8523b40a97698b26adc
Pierre <pierrezhoou@gmail.com>
2020-01-12 18:03:14 +0100

    add contact

---
+++ app/src/main/java/fr/firstapp/BlocNote/Model/Person.kt
@@ -0,0 +1,16 @@
+package fr.firstapp.BlocNote.Model
+
+class Person {
+    var id : Int = 0
+    var name : String ?= null
+    var email : String ?= null
+
+    constructor(){}
+
+    constructor(id : Int, name : String, email : String) {
+        this.id = id
+        this.name = name
+        this.email = email
+    }
+
+}
